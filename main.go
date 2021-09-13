f72f2710501005f73d744d73abfc1c4b7e0db023
SeanQO <seanquintero3@gmail.com>
2021-09-13 05:26:11 -0500

    added login and sign in functionality

--- main.go
+++ main.go
@@ -5,57 +5,75 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"time"
+	"strings"
 )
 
 type person struct {
 	username  string
+	password  string
 	fname     string
-	lName     string
-	birthDate time.Time
+	lname     string
+	birthDate string
+}
+
+type Data struct {
+	Items []person
 }
 
 var users = []*person{}
 
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("methodLogin:", r.Method) //get request method
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("login.html")
-		t.Execute(w, nil)
-	} else {
-		r.ParseForm()
-		// logic part of log in
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
+	r.ParseForm()
+	// logic part of log in
 
+	uname := strings.Join(r.Form["username"], " ")
+	psw := strings.Join(r.Form["password"], " ")
+
+	for _, p := range users {
+		fmt.Println("huhuh")
+		if p.username == uname && p.password == psw {
+			fmt.Println("p: " + p.username + " " + p.password)
+			fmt.Println("************************************************************")
+			http.Redirect(w, r, "/signIn", http.StatusFound)
+
+		}
 	}
+
+	t, _ := template.ParseFiles("login.html")
+	t.Execute(w, nil)
 }
 
 func signUp(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("methodSignUP:", r.Method)
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("signUp.html")
-		t.Execute(w, nil)
-	} else {
-		r.ParseForm()
+	t, _ := template.ParseFiles("signUp.html")
+	t.Execute(w, nil)
+	r.ParseForm()
+	username := strings.Join(r.Form["username"], " ")
+	password := strings.Join(r.Form["psw"], " ")
+	fname := strings.Join(r.Form["fname"], " ")
+	lname := strings.Join(r.Form["lname"], " ")
+	birthDate := strings.Join(r.Form["birthDate"], " ")
+	p := person{username: username, password: password, fname: fname, lname: lname, birthDate: birthDate}
+	users = append(users, &p)
 
-	}
+	http.Redirect(w, r, "/login", http.StatusFound)
 
 }
 
 func signIn(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("methodSignIn:", r.Method)
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("signIn.html")
-		t.Execute(w, nil)
-	} else {
-		r.ParseForm()
-
+	t, _ := template.ParseFiles("signIn.html")
+	t.Execute(w, nil)
+	for _, p := range users {
+		fmt.Println("UN: " + p.username + "PS: " + p.password)
 	}
+	r.ParseForm()
 
 }
 
 func main() {
+	users = append(users, &person{username: "admin", password: "admin", fname: "admin", lname: "admin", birthDate: "3/3/3"})
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/signUp", signUp)
 	http.HandleFunc("/signIn", signIn)
